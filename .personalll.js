a1bf0807a432743cc147a679146a65cbaf912e04
NayanSr <nayankbps@gmail.com>
2023-11-27 02:51:15 +0600

    basic-setupped-next-app-with-basic-nav&conternt&footer-component

---
+++ .personalll.js
@@ -0,0 +1,13 @@
+/* 
+1. npx create-next-app@13.4.9
+2. change font(in layout.tsx 24:00) >  
+     const playpenSans = Playpen_Sans({
+       subsets: ["latin"],
+       weight: ["400", "700"],
+     });
+3. Add nav bar and footer 28:00 
+4. Keeping content in full hight of the screen between nav and footer (all 3 parent to flex flex-col min-h-screen and middle content div to flex-grow)
+
+
+
+*/
