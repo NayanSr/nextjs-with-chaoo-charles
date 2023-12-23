af24e72dd32821aabc2510841d056b63e60f241d
NayanSr <nayankbps@gmail.com>
2023-12-23 19:21:04 +0600

    removing-added-item-from-cart

--- src/app/cart/ItemContent.tsx
+++ src/app/cart/ItemContent.tsx
@@ -4,12 +4,14 @@ import { truncateText } from "../../../utils/truncateText";
 import { formatePrice } from "../../../utils/formatePrice";
 import Image from "next/image";
 import { SetQuantity } from "../components/products/SetQuantity";
+import { useCart } from "@/hooks/useCart";
 
 interface ItemContentProps {
   item: CartProductType;
 }
 
 const ItemContent: React.FC<ItemContentProps> = ({ item }) => {
+  const { handleRemoveProductFromCart } = useCart();
   return (
     <div className="grid grid-cols-5 gap-5 text-xs md:text-sm  border-t-[2px] border-orange-400 py-4 items-center">
       <div className="flex col-span-2 justify-self-start gap-2 md:gap-4">
@@ -31,7 +33,12 @@ const ItemContent: React.FC<ItemContentProps> = ({ item }) => {
             {item.selectedImg.color}
           </div>
           <div className="max-w-[75px]">
-            <button className="text-slate-700 underline">Remove</button>
+            <button
+              className="text-slate-700 underline"
+              onClick={() => handleRemoveProductFromCart(item)}
+            >
+              Remove
+            </button>
           </div>
         </div>
       </div>
